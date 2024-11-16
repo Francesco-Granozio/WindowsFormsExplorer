906425fc62cee6fa30550743a6e0487a6171600f
Francesco Granozio <59570638+Francesco-Granozio@users.noreply.github.com>
2024-11-16 14:39:56 +0100

    code refactor

--- WindowsFormsExplorer/WindowsFormsExplorer/MainForm.Designer.cs
+++ WindowsFormsExplorer/WindowsFormsExplorer/MainForm.Designer.cs
@@ -3,59 +3,133 @@ using System;
 
 namespace WindowsFormsExplorer
 {
-    public partial class MainForm
+    public partial class MainForm : Form
     {
         private TextBox txtPID;
         private Button btnConnect;
         private Button btnRefresh;
         private ListView listViewForms;
+        private TreeView treeViewControls;
+        private SplitContainer splitContainer;
+
 
         private void InitializeComponent()
         {
-            this.txtPID = new TextBox();
-            this.btnConnect = new Button();
-            this.btnRefresh = new Button();
-            this.listViewForms = new ListView();
-
-            // Form
-            this.Text = "Form Inspector";
-            this.Size = new System.Drawing.Size(800, 600);
-
-            // PID TextBox
+            this.txtPID = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.listViewForms = new System.Windows.Forms.ListView();
+            this.treeViewControls = new System.Windows.Forms.TreeView();
+            this.controlsPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.controlsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPID
+            // 
             this.txtPID.Location = new System.Drawing.Point(12, 12);
+            this.txtPID.Name = "txtPID";
             this.txtPID.Size = new System.Drawing.Size(100, 20);
-            this.Controls.Add(this.txtPID);
-
-            // Connect Button
+            this.txtPID.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
             this.btnConnect.Location = new System.Drawing.Point(118, 10);
+            this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
             this.btnConnect.Text = "Connetti";
-            this.btnConnect.Click += new EventHandler(btnConnect_Click);
-            this.Controls.Add(this.btnConnect);
-
-            // Refresh Button
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnRefresh
+            // 
             this.btnRefresh.Location = new System.Drawing.Point(199, 10);
+            this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 2;
             this.btnRefresh.Text = "Aggiorna";
-            this.btnRefresh.Click += new EventHandler(btnRefresh_Click);
-            this.Controls.Add(this.btnRefresh);
-
-            // ListView
-            this.listViewForms.Location = new System.Drawing.Point(12, 45);
-            this.listViewForms.Size = new System.Drawing.Size(760, 500);
-            this.listViewForms.View = View.Details;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.listViewForms);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.treeViewControls);
+            this.splitContainer.Size = new System.Drawing.Size(1184, 716);
+            this.splitContainer.SplitterDistance = 592;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // listViewForms
+            // 
+            this.listViewForms.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listViewForms.FullRowSelect = true;
             this.listViewForms.GridLines = true;
-
-            // Aggiungi colonne
+            this.listViewForms.HideSelection = false;
+            this.listViewForms.Location = new System.Drawing.Point(0, 0);
+            this.listViewForms.Name = "listViewForms";
+            this.listViewForms.Size = new System.Drawing.Size(592, 716);
+            this.listViewForms.TabIndex = 0;
+            this.listViewForms.UseCompatibleStateImageBehavior = false;
+            this.listViewForms.View = System.Windows.Forms.View.Details;
+            this.listViewForms.SelectedIndexChanged += new System.EventHandler(this.listViewForms_SelectedIndexChanged);
             this.listViewForms.Columns.Add("Nome", 150);
             this.listViewForms.Columns.Add("Tipo", 200);
             this.listViewForms.Columns.Add("Testo", 200);
             this.listViewForms.Columns.Add("Visibile", 100);
             this.listViewForms.Columns.Add("Handle", 100);
 
-            this.Controls.Add(this.listViewForms);
+            // 
+            // treeViewControls
+            // 
+            this.treeViewControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewControls.Location = new System.Drawing.Point(0, 0);
+            this.treeViewControls.Name = "treeViewControls";
+            this.treeViewControls.Size = new System.Drawing.Size(588, 716);
+            this.treeViewControls.TabIndex = 0;
+            // 
+            // controlsPanel
+            // 
+            this.controlsPanel.Controls.Add(this.txtPID);
+            this.controlsPanel.Controls.Add(this.btnConnect);
+            this.controlsPanel.Controls.Add(this.btnRefresh);
+            this.controlsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.controlsPanel.Location = new System.Drawing.Point(0, 0);
+            this.controlsPanel.Name = "controlsPanel";
+            this.controlsPanel.Size = new System.Drawing.Size(1184, 45);
+            this.controlsPanel.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1184, 761);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.controlsPanel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Inspector";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.controlsPanel.ResumeLayout(false);
+            this.controlsPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
+
+        private Panel controlsPanel;
     }
 
 }
--- WindowsFormsExplorer/WindowsFormsExplorer/MainForm.cs
+++ WindowsFormsExplorer/WindowsFormsExplorer/MainForm.cs
@@ -12,17 +12,23 @@ namespace WindowsFormsExplorer
         private DTE2 debuggerInstance;
         private System.Diagnostics.Process targetProcess;
 
+
         public MainForm()
         {
             InitializeComponent();
         }
 
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+
         private void btnConnect_Click(object sender, EventArgs e)
         {
             try
             {
-                int pid;
-                if (!int.TryParse(txtPID.Text, out pid))
+                if (!int.TryParse(txtPID.Text, out int pid))
                 {
                     MessageBox.Show("PID non valido!");
                     return;
@@ -37,10 +43,12 @@ namespace WindowsFormsExplorer
             }
         }
 
+
         private void ConnectToProcess(int pid)
         {
             // Ottiene il processo target
             targetProcess = System.Diagnostics.Process.GetProcessById(pid);
+
             if (targetProcess == null)
             {
                 throw new Exception("Processo non trovato!");
@@ -73,6 +81,7 @@ namespace WindowsFormsExplorer
             }
         }
 
+
         private void RefreshOpenForms()
         {
             listViewForms.Items.Clear();
@@ -95,73 +104,63 @@ namespace WindowsFormsExplorer
                     throw new Exception("Processo debuggato non trovato!");
                 }
 
-                // Accedi alla collezione threads in modo sicuro
+                // Accedo alla collezione threads in modo sicuro
                 try
                 {
-                    // Selezioniamo il primo thread disponibile
+                    // Seleziono il primo thread disponibile
                     Processes threads = debuggedProcess.Collection;
-                    if (threads.Count > 0)
+                    if (threads.Count <= 0)
                     {
-                        // Seleziona il primo thread
-                        EnvDTE.Process mainThread = threads.Item(1);
+                        throw new Exception("Nessun thread disponibile nel processo!");
+                    }
 
-                        // Assicuriamoci che il debugger sia in pausa
-                        if (debuggerInstance.Debugger.CurrentMode == dbgDebugMode.dbgBreakMode)
+                    // Seleziona il primo thread
+                    EnvDTE.Process mainThread = threads.Item(1);
+
+                    // Il debugger deve essere in pausa
+                    if (debuggerInstance.Debugger.CurrentMode != dbgDebugMode.dbgBreakMode)
+                    {
+                        throw new Exception("Il debugger deve essere in modalità break!");
+                    }
+
+
+                    string formCountStr = GetExpressionValue("System.Windows.Forms.Application.OpenForms.Count");
+
+
+                    if (string.IsNullOrEmpty(formCountStr))
+                    {
+                        throw new Exception("Impossibile ottenere il conteggio dei forms!");
+                    }
+
+                    if (!int.TryParse(formCountStr, out int formCount))
+                    {
+                        throw new Exception($"Valore conteggio non valido: {formCountStr}");
+                    }
+
+                    for (int i = 0; i < formCount; i++)
+                    {
+                        string baseExpr = $"System.Windows.Forms.Application.OpenForms[{i}]";
+
+                        Expression formExpr = debuggerInstance.Debugger.GetExpression(baseExpr);
+                        if (formExpr == null)
                         {
-                            // Valuta l'espressione per ottenere il conteggio dei forms
-                            Expression countExpr = debuggerInstance.Debugger.GetExpression(
-                                "System.Windows.Forms.Application.OpenForms.Count",
-                                false, // No side effects
-                                30000 // 30 secondi timeout
-                            );
+                            continue;
+                        }
 
-                            if (countExpr != null && !string.IsNullOrEmpty(countExpr.Value))
-                            {
-                                int formCount;
-                                if (int.TryParse(countExpr.Value, out formCount))
-                                {
-                                    for (int i = 0; i < formCount; i++)
-                                    {
-                                        string baseExpr = $"System.Windows.Forms.Application.OpenForms[{i}]";
+                        string text = GetExpressionValue($"{baseExpr}.Text") ?? "N/A";
+                        string type = formExpr.Type ?? "N/A";
+                        string visible = GetExpressionValue($"{baseExpr}.Visible") ?? "N/A";
+                        string handle = GetExpressionValue($"{baseExpr}.Handle.ToInt32()") ?? "N/A";
+                        string name = GetExpressionValue($"{baseExpr}.Name") ?? $"Form_{i}";
 
-                                        Expression formExpr = debuggerInstance.Debugger.GetExpression(baseExpr);
-                                        if (formExpr != null)
-                                        {
-                                            string text = GetExpressionValue($"{baseExpr}.Text") ?? "N/A";
-                                            string type = formExpr.Type ?? "N/A";
-                                            string visible = GetExpressionValue($"{baseExpr}.Visible") ?? "N/A";
-                                            string handle = GetExpressionValue($"{baseExpr}.Handle.ToInt32()") ?? "N/A";
-                                            string name = GetExpressionValue($"{baseExpr}.Name") ?? $"Form_{i}";
-
-                                            ListViewItem item = new ListViewItem(new string[] {
+                        ListViewItem item = new ListViewItem(new string[] {
                                                 name,
                                                 type,
                                                 text,
                                                 visible,
                                                 handle
                                             });
-                                            listViewForms.Items.Add(item);
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    throw new Exception($"Valore conteggio non valido: {countExpr.Value}");
-                                }
-                            }
-                            else
-                            {
-                                throw new Exception("Impossibile ottenere il conteggio dei forms!");
-                            }
-                        }
-                        else
-                        {
-                            throw new Exception("Il debugger deve essere in modalità break!");
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("Nessun thread disponibile nel processo!");
+                        listViewForms.Items.Add(item);
                     }
                 }
                 catch (Exception ex)
@@ -176,23 +175,200 @@ namespace WindowsFormsExplorer
         }
 
 
+        private void listViewForms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewForms.SelectedItems.Count > 0)
+            {
+                int formIndex = listViewForms.SelectedItems[0].Index;
+                ExploreFormControls(formIndex);
+            }
+        }
 
-        private string GetExpressionValue(string expression)
+
+        private void ExploreFormControls(int formIndex)
+        {
+            treeViewControls.Nodes.Clear();
+
+            try
+            {
+                string baseExpr = $"System.Windows.Forms.Application.OpenForms[{formIndex}]";
+                Expression formExpr = debuggerInstance.Debugger.GetExpression(baseExpr);
+
+                if (formExpr != null)
+                {
+                    TreeNode rootNode = new TreeNode(GetExpressionValue($"{baseExpr}.Name") ?? $"Form_{formIndex}")
+                    {
+                        Tag = baseExpr
+                    };
+                    ExploreControlsRecursively(baseExpr, rootNode);
+                    treeViewControls.Nodes.Add(rootNode);
+                    rootNode.Expand();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante l'esplorazione dei controlli: {ex.Message}");
+            }
+        }
+
+
+        private void ExploreControlsRecursively(string controlExpr, TreeNode parentNode)
+        {
+
+            try
+            {
+                // Ottieni la collezione Controls
+                Expression controlsExpr = debuggerInstance.Debugger.GetExpression(
+                    $"{controlExpr}.Controls",
+                    false,
+                    30000
+                );
+
+                if (controlsExpr != null)
+                {
+                    // Ottieni il conteggio dei controlli
+                    Expression countExpr = debuggerInstance.Debugger.GetExpression(
+                        $"{controlExpr}.Controls.Count",
+                        false,
+                        30000
+                    );
+
+                    if (countExpr != null && int.TryParse(countExpr.Value, out int controlCount))
+                    {
+                        for (int i = 0; i < controlCount; i++)
+                        {
+                            string childExpr = $"{controlExpr}.Controls[{i}]";
+
+                            // Ottieni informazioni sul controllo
+                            string name = GetExpressionValue($"{childExpr}.Name") ?? $"Control_{i}";
+                            string type = GetExpressionValue($"{childExpr}.GetType().Name") ?? "Unknown";
+                            string text = GetExpressionValue($"{childExpr}.Text") ?? "";
+                            bool visible = GetExpressionValue($"{childExpr}.Visible") == "true";
+
+                            // Crea il nodo per questo controllo
+                            string nodeText = $"{name} ({type})";
+                            if (!string.IsNullOrEmpty(text))
+                            {
+                                nodeText += $" - '{text}'";
+                            }
+                            if (!visible)
+                            {
+                                nodeText += " [Hidden]";
+                            }
+
+                            TreeNode node = new TreeNode(nodeText)
+                            {
+                                Tag = childExpr
+                            };
+
+                            // Aggiungi proprietà aggiuntive
+                            //AddControlProperties(childExpr, node);
+
+                            // Ricorsione sui controlli figli
+                            ExploreControlsRecursively(childExpr, node);
+
+                            parentNode.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TreeNode errorNode = new TreeNode($"Error: {ex.Message}");
+                parentNode.Nodes.Add(errorNode);
+            }
+        }
+
+
+        private void AddControlProperties(string controlExpr, TreeNode node)
         {
             try
             {
-                Expression expr = debuggerInstance.Debugger.GetExpression(
-                    expression,
-                    false,  // No side effects
-                    30000   // Timeout 30 secondi
-                );
-                return expr?.Value;
+                // Lista delle proprietà comuni da ispezionare
+                var properties = new[]
+                {
+                    "Location", "Size", "Dock", "Anchor", "TabIndex", "Enabled",
+                    "BackColor", "ForeColor", "Font", "Padding", "Margin"
+                };
+
+                foreach (var prop in properties)
+                {
+                    string value = GetExpressionValue($"{controlExpr}.{prop}");
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        TreeNode propNode = new TreeNode($"{prop}: {value}");
+                        node.Nodes.Add(propNode);
+                    }
+                }
+
+                // Proprietà specifiche per diversi tipi di controlli
+                string type = GetExpressionValue($"{controlExpr}.GetType().Name");
+                switch (type)
+                {
+                    case "TextBox":
+                        AddProperty(node, controlExpr, "MultiLine");
+                        AddProperty(node, controlExpr, "MaxLength");
+                        AddProperty(node, controlExpr, "ReadOnly");
+                        break;
+                    case "Button":
+                        AddProperty(node, controlExpr, "DialogResult");
+                        AddProperty(node, controlExpr, "FlatStyle");
+                        break;
+                    case "ComboBox":
+                        AddProperty(node, controlExpr, "DropDownStyle");
+                        AddProperty(node, controlExpr, "Items.Count");
+                        AddProperty(node, controlExpr, "SelectedIndex");
+                        break;
+                        // altri casi specifici per tipo...
+                }
             }
-            catch
+            catch (Exception ex)
             {
-                return null;
+                TreeNode errorNode = new TreeNode($"Error getting properties: {ex.Message}");
+                node.Nodes.Add(errorNode);
             }
         }
+
+
+        private void AddProperty(TreeNode node, string controlExpr, string propertyName)
+        {
+            string value = GetExpressionValue($"{controlExpr}.{propertyName}");
+            if (!string.IsNullOrEmpty(value))
+            {
+                TreeNode propNode = new TreeNode($"{propertyName}: {value}");
+                node.Nodes.Add(propNode);
+            }
+        }
+
+
+        private string GetExpressionValue(string expression)
+        {
+            int retryCount = 3;
+            int delayBetweenRetries = 100; // Millisecondi
+
+            for (int i = 0; i < retryCount; i++)
+            {
+                try
+                {
+                    Expression expr = debuggerInstance.Debugger.GetExpression(expression, false, 300000);
+                    return expr?.Value;
+                }
+                catch (COMException ex) when ((uint)ex.HResult == 0x80010001) // RPC_E_CALL_REJECTED
+                {
+                    if (i == retryCount - 1)
+                    {
+                        Console.WriteLine($"Eccezione durante il recupero dell'espressione: {ex.Message}");
+                        throw; // Rilancia l'eccezione se siamo all'ultimo tentativo
+                    }
+
+                    // Aspetta prima del prossimo tentativo
+                    System.Threading.Thread.Sleep(delayBetweenRetries);
+                }
+            }
+            return null;
+        }
+
+
 
         private void btnRefresh_Click(object sender, EventArgs e)
         {
@@ -206,6 +382,7 @@ namespace WindowsFormsExplorer
             }
         }
 
+
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             base.OnFormClosing(e);
@@ -215,6 +392,9 @@ namespace WindowsFormsExplorer
                 Marshal.ReleaseComObject(debuggerInstance);
             }
         }
+
+
+
     }
 
 }
