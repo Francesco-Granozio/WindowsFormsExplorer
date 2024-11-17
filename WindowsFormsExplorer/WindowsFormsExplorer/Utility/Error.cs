cefe65af133ecb189f642e1d688e57f5f62f0386
Francesco Granozio <59570638+Francesco-Granozio@users.noreply.github.com>
2024-11-17 11:34:44 +0100

    process connector

---
+++ WindowsFormsExplorer/WindowsFormsExplorer/Utility/Error.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace WindowsFormsExplorer.Utility
+{
+    public enum ErrorCode
+    {
+        None,
+        Exception,
+        NoVSIstanceFound,
+        NoInstanceSelected,
+        NoDebugProcessFound,
+        InvaliidPID,
+        ProcessMustBeInDebugMode,
+        DebuggerConnectionError
+    }
+
+
+    public class Error
+    {
+        public Error(ErrorCode code, string message, Exception exception)
+        {
+            Code = code;
+            Message = message;
+            Exception = exception;
+        }
+
+        public Error(ErrorCode code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public ErrorCode Code { get; }
+        
+        public string Message { get; }
+        
+        public Exception Exception { get; }
+        
+        public bool HasException => Exception != null;
+
+        public static Error None => new Error(ErrorCode.None, string.Empty);
+
+
+    }
+
+}
---
+++ WindowsFormsExplorer/WindowsFormsExplorer/Utility/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsExplorer.Utility
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public T Value { get; }
+        public Error Error { get; }
+
+        private Result(bool isSuccess, T value, Error error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+
+        public static Result<T> Success(T value) =>
+        new Result<T>(true, value, Error.None);
+
+        public static Result<T> Failure(Error error) =>
+            new Result<T>(false, default, error);
+
+        public static implicit operator Result<T>(T value) =>
+            Success(value);
+
+        public static implicit operator Result<T>(Error error) =>
+            Failure(error);
+
+
+        public Result<TResult> Map<TResult>(Func<T, TResult> func)
+        {
+            if (IsFailure)
+                return Result<TResult>.Failure(Error);
+
+            return Result<TResult>.Success(func(Value));
+        }
+
+
+        public Result<TResult> Bind<TResult>(Func<T, Result<TResult>> func)
+        {
+            if (IsFailure)
+                return Result<TResult>.Failure(Error);
+
+            return func(Value);
+        }
+
+
+    }
+
+
+
+}
